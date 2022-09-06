636e9a4e7c53a9880b0bcd86a02a7c1979e0e941
Harrison Preusse <hpreusse@gmail.com>
2022-09-06 10:09:55 -0400

    Update Exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,5 @@
-const removeFromArray = function(arr, ...toRemove) {
+const removeFromArray = function(...args) {
+  /*
   //spread notation (...toRemove) turns all optional arguments into an array
   //forEach element of array toRemove, do this
   toRemove.forEach((x) => {
@@ -10,6 +11,18 @@ const removeFromArray = function(arr, ...toRemove) {
       arr.splice(index, 1);
   });
   return arr;
+  */
+
+  //Get array (first argument provided)
+  const arr = args[0];
+  //new blank array
+  const newArr = [];
+
+  arr.forEach((item) => {
+    if(!args.includes(item))
+      newArr.push(item);
+  });
+  return newArr;
 }
 
 // Do not edit below this line
