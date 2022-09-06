7a73960c42ea83defd6b2b06444130a6e755b4c7
Harrison Preusse <hpreusse@gmail.com>
2022-09-06 10:02:54 -0400

    Finish Exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,16 @@
-const removeFromArray = function() {
-
-};
+const removeFromArray = function(arr, ...toRemove) {
+  //spread notation (...toRemove) turns all optional arguments into an array
+  //forEach element of array toRemove, do this
+  toRemove.forEach((x) => {
+    //find index of item
+    const index = arr.indexOf(x);
+    //only remove if index found (will ignore values not in arr)
+    if (index > -1)
+      //only removes 1 character at index
+      arr.splice(index, 1);
+  });
+  return arr;
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
