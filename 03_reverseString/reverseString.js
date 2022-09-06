a4f9508e11250375352d010a978579db70198f38
Harrison Preusse <hpreusse@gmail.com>
2022-09-06 09:43:14 -0400

    Update Exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,10 +1,5 @@
 const reverseString = function(string) {
-  let reversed = '';
-  let letters = string.split('');
-  letters = letters.reverse();
-  reversed = letters.toString();
-  reversed = reversed.replaceAll(',', '');
-  return reversed;
+  return string.split('').reverse().join('');
 };
 
 // Do not edit below this line
