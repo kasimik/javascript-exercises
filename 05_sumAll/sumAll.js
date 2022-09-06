b85d58f1b79d0686c0a786e1ade8d70b74cd03a7
Harrison Preusse <hpreusse@gmail.com>
2022-09-06 10:25:46 -0400

    Finish Exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,28 @@
-const sumAll = function() {
+const sumAll = function(x, y) {
+  let finalSum = null;
+  let first = 0;
+  let last = 0;
 
+  if (typeof x === 'number' && typeof y === 'number' && x >= 0 && y >= 0) {
+    if (x <= y) {
+      first = x;
+      last = y;
+    }
+    else {
+      first = y;
+      last = x;
+    }
+    for (first; first <= last; first++) {
+      finalSum += first;
+    }
+  }
+  else {
+    finalSum = 'ERROR';
+  }
+  
+
+  
+  return finalSum;
 };
 
 // Do not edit below this line
